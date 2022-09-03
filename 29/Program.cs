9be7d92f5475a5e6e714f9a23123d961b8b0ea0f
Leisan <dororoqw@gmail.com>
2022-09-04 00:44:15 +0300

    29 задача

--- 29/Program.cs
+++ 29/Program.cs
@@ -2,17 +2,42 @@
 // 1, 2, 5, 7, 19 -> [1, 2, 5, 7, 19]
 // 6, 1, 33 -> [6, 1, 33]
 
-int [] numbers = new int[8];
-Console.Write("[");
 
-for (int i = 0; i < numbers.Length; i++)
- {
-    numbers [i] = new Random().Next(0, 11);
-    Console.Write(" " + Method (i) + " ");
- }
-Console.Write("]");
 
-int Method (int a)
+class Program
 {
-    return numbers[a];
+    static void WriteArray(int[] array)
+    {
+        for (int i = 0; i < array.Length; i++)
+        {
+            Console.Write(array[i] + " ");
+        }
+    }
+
+    static void Main(string[] args)
+    {
+        int[] array = new int[0];
+        while (true)
+        {
+            Console.WriteLine("Введите значение массива: ");
+            string meaning = Console.ReadLine();
+            if (meaning != "end")
+            {
+                int number = Convert.ToInt32(meaning);
+                int[] newArray = array;
+                array = new int[array.Length + 1];
+                for (int i = 0; i < newArray.Length; i++)
+                {
+                    array[i] = newArray[i];
+                }
+                array[array.Length - 1] = number;
+            }
+            else
+            {
+                break;
+            }
+
+        }
+        WriteArray(array);
+    }
 }
